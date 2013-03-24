39468f281a5374778c8f279fecd6a72c22b41d83
indigoparadox <indigoparadox+bitbucket@interfinitdynamics.info>
2013-03-24 14:31:48 -0400

    Switched maps over to TMX format, now hammering out the specs.

---
+++ tilesets/tallgrass.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tallgrass" tilewidth="32" tileheight="32">
+ <image source="../assets/submission_daneeklu/tilesets/tallgrass.png" width="96" height="192"/>
+</tileset>
